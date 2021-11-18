3f82524f205ae5313816f12b502b27f87e93c10c
sergey1701 <73608392+sergey1701@users.noreply.github.com>
2021-11-18 21:15:52 +0200

    Simple Terraform Instruction

--- Terraform/test.tf
+++ Terraform/test.tf
@@ -1,7 +1,7 @@
 provider "aws"{
     access_key = "AKIA3A77BKJKBLLKVP67"
     secret_key = "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"
-    region = "eu-central-1"
+    region = "us-east-2"
 }
 
 resource "aws_instance" "myweb" {
