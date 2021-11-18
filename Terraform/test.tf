0dd2e49da895052ec7ec72b3a9a3abc760770aa8
sergey1701 <73608392+sergey1701@users.noreply.github.com>
2021-11-18 20:48:31 +0200

    Simple Terraform Instruction

--- Terraform/test.tf
+++ Terraform/test.tf
@@ -1,6 +1,6 @@
 provider "aws"{
-    access_key = AKIA3A77BKJKISRO3WAD
-    secret_key = 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
+    access_key = "AKIA3A77BKJKBLLKVP67"
+    secret_key = "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"
     region = "eu-central-1"
 }
 
