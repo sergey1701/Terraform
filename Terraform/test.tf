75749862967f38e52f262a64531c921efb3efe27
sergey1701 <73608392+sergey1701@users.noreply.github.com>
2021-11-19 22:31:53 +0200

    Deliting Machine

--- Terraform/test.tf
+++ Terraform/test.tf
@@ -1,6 +1,6 @@
 provider "aws"{
-    access_key = "${env.AWS_ACCESS_KEY_ID}"
-    secret_key = "${env.AWS_SECRET_ACCESS_KEY}"
+    access_key = ACCESS_KEY_ID
+    secret_key = SECRET_KEY
     region = "us-east-2"
 }
 
