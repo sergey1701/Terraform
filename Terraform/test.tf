497616f431bb63459e141906cdecb83e2b50bb8f
sergey1701 <73608392+sergey1701@users.noreply.github.com>
2021-11-20 09:01:33 +0200

    Deliting Machine

--- Terraform/test.tf
+++ Terraform/test.tf
@@ -4,7 +4,7 @@ provider "aws"{
     region = "us-east-2"
 }
 
-resource "aws_instance" "myweb" {
+resource "aws_instance" "test" {
   count = 2  
   ami           = "ami-0629230e074c580f2"
   instance_type = "t2.micro"
